f685a4f0b02d840bb472e64272c4608c2b929a8b
Ryan Larson <ryan-p-larson@uiowa.edu>
2015-12-13 19:23:31 -0600

    Working, but unformatted scraper

--- data/scraper.py
+++ data/scraper.py
@@ -4,21 +4,28 @@ import csv
 
 rankings = {}
 counter = 0
-# for i in range(1,16):
+for i in range(1,16):
+    week = i
 
-url = 'http://espn.go.com/college-football/rankings/_/poll/seasontype/2/year/2015/week/1'
-page = urlopen(url).read()
-soup = BeautifulSoup(page)
+    url = 'http://espn.go.com/college-football/rankings/_/poll/seasontype/2/year/2015/week/{}'.format(week)
+    page = urlopen(url).read()
+    soup = BeautifulSoup(page)
 
-table = soup.find('table')
-for row in table.find_all('tr')[1:26]:
-    col = row.find_all('td')
+    table = soup.find('table')
+    for row in table.find_all('tr')[1:26]:
+        col = row.find_all('td')
 
-    rank = col[0].text
-    team = col[1].text
-    record = col[2].text
+        rank = col[0].text
+        team = col[1].text
+        record = col[2].text
 
-    rankings[counter] = {"rank":rank, "team":team, "record":record}
-    counter += 1
+        rankings[counter] = {"week":week, "rank":rank, "team":team, "record":record}
+        counter += 1
 
-print (rankings)
+with open("2015-rankings.csv", "wb") as outfile:
+    attrNames = ["rank", "team", "record", "week"]
+    writer = csv.DictWriter(outfile, fieldnames = attrNames)
+
+    writer.writeheader()
+    for row in rankings:
+        writer.writerow(rankings[row])
